4515a3bd1617d383269a0869a57fd071b942ef78
pduo0004 <144980792+pduo0004@users.noreply.github.com>
2023-10-08 18:04:09 +1100

    Delete Week10Homework directory

--- Week10Homework/week10_vis.js
+++
@@ -1,9 +0,0 @@
-var vg_1 = "week9homework.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
-
-}).catch(console.error);
-
-var vg_2 = "week9homework3.vg.json";
-vegaEmbed("#line_chart", vg_2).then(function(result) {
-
-}).catch(console.error);
